ee6e2689987f05bb4ac5d24f6f680a154a8bbf65
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-21 23:49:59 +0900

    エスケープ命令の実装

--- staff_add_check.php
+++ staff_add_check.php
@@ -12,6 +12,10 @@ $staff_name = $POST['name'];
 $staff_pass = $POST['pass'];
 $staff_pass2 = $POST['pass2'];
 
+$staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
+$staff_pass = htmlspecialchars($staff_pass, ENT_QUOTES, 'UTF-8');
+$staff_pass2 = htmlspecialchars($staff_pass2, ENT_QUOTES, 'UTF-8');
+
 
 
 ?>
