b2426c27ea9a2ae21610af9ba57463ce73f43aa0
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-22 00:44:25 +0900

    入力値受け渡しに関する修正

--- staff_add_check.php
+++ staff_add_check.php
@@ -6,15 +6,15 @@
 </head>
 <body>
 <?php
-$staff_name = $POST['name'];
-$staff_pass = $POST['pass'];
-$staff_pass2 = $POST['pass2'];
+$staff_name = $_POST['name'];
+$staff_pass = $_POST['pass'];
+$staff_pass2 = $_POST['pass2'];
 
 $staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
 $staff_pass = htmlspecialchars($staff_pass, ENT_QUOTES, 'UTF-8');
 $staff_pass2 = htmlspecialchars($staff_pass2, ENT_QUOTES, 'UTF-8');
 
-if(staff_name == '') {
+if($staff_name == '') {
     print 'スタッフ名が入力されていません。<br />';
 } else {
     print 'スタッフ名：';
