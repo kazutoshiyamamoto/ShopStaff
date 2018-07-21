f131c3bfe5edec5783bfedb9625932a6487bb9ed
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-21 21:03:55 +0900

    スタッフ名を追加するフォームの実装

--- staff_add.php
+++ staff_add.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>ろくまる農園</title>
+<title>スタッフ追加フォーム</title>
 </head>
 <body>
+
+スタッフ追加</ br>
+<br />
+<form method = "post" action = "staff_add_check.php">
+スタッフ名を入力してください。<br />
+<input type = "text" name = "name" style = "width:200px"><br />
+
+</form>
 
 </body>
 </html>
