5f57e0ae4195b624a3ab71d030143c57dafde552
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-21 21:04:35 +0900

    パスワード入力欄の実装

--- staff_add.php
+++ staff_add.php
@@ -11,8 +11,12 @@
 <form method = "post" action = "staff_add_check.php">
 スタッフ名を入力してください。<br />
 <input type = "text" name = "name" style = "width:200px"><br />
+パスワードを入力してください。<br />
+<input type = "password" name = "pass" style = "width:100px"><br />
+パスワードをもう1度入力してください。<br />
+<input type = "password" name = "pass2" style = "width:100px"><br />
+<br />
 
-</form>
 
 </body>
 </html>
