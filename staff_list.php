d854aca5e07957f1adeb2204039cfb64202aa4d2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-24 21:31:01 +0900

    データベースに接続する

--- staff_list.php
+++ staff_list.php
@@ -6,5 +6,21 @@
 </head>
 <body>
 
+<?php
+
+try {
+    $dsn = 'mysql:dbname = shop;host = localhost;charset = utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+} catch {
+
+}
+
+?>
+
 </body>
 </html>
