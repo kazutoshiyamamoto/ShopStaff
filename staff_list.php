c59610c1ed10a070dce4090713cbe4ecc645fe6e
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-25 20:26:18 +0900

    スタッフ一覧を修正できるよう変更

--- staff_list.php
+++ staff_list.php
@@ -15,7 +15,7 @@ try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT name FROM mst_staff WHERE 1';
+    $sql = 'SELECT code, name FROM mst_staff WHERE 1';
     $stmt = $dbh -> prepare($sql);
     $stmt -> execute();
 
@@ -23,14 +23,19 @@ try {
 
     print 'スタッフ一覧<br /><br />';
 
+    print '<form method = "post" action = "staff_edit.php">';
+
     while(true) {
         $rec = $stmt -> fetch(PDO::FETCH_ASSOC);
         if($rec == false) {
             break;
         }
+        print '<input type = "radio" name = "staffcode" value = "'.$rec['code'].'">';
         print $rec['name'];
         print '<br />';
     }
+    print '<input type = "submit" value = "修正">';
+    print '</form>';
 
 } catch(Exception $e) {
     print 'ただいま障害により大変ご迷惑をおかけしております。';
