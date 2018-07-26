31420dbf513930b93d253545711414332ec93cf3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-27 08:22:38 +0900

    データベース接続に関する実装

--- staff_edit.php
+++ staff_edit.php
@@ -10,6 +10,12 @@
 
 try {
     $staff_code = $_POST['staffcode'];
+
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
 
 
