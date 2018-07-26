df8e6fffd3b8e5896fd37a6ecf6e5a02d8f2e276
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-27 08:14:55 +0900

    スタッフ修正画面を実装するファイルを追加

--- staff_add_done.php
+++ staff_add_done.php
@@ -14,22 +14,24 @@ try {
     $staff_name = htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8');
     $staff_pass = htmlspecialchars($staff_pass, ENT_QUOTES, 'UTF-8');
 
-    $dsn = 'mysql:dbname = shop;host = localhost;charset = utf8';
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
     $user = 'root';
     $password = '';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'INSERT INTO mst_staff(name, password) VALUES (?,?)';
-    $stmt = $dbh -> prepare($sql);
-    $data[] = $staff_name;
-    $data[] = $staff_pass;
-    $stmt -> execute($data);
+    print $dbh;
 
-    $dbh = null;
+    // $sql = 'INSERT INTO mst_staff(name, password) VALUES (?,?)';
+    // $stmt = $dbh -> prepare($sql);
+    // $data[] = $staff_name;
+    // $data[] = $staff_pass;
+    // $stmt -> execute($data);
 
-    print $staff_name;
-    print 'さんを追加しました。<br />';
+    // $dbh = null;
+
+    // print $staff_name;
+    // print 'さんを追加しました。<br />';
 
 }
 catch (Exception $e) 
