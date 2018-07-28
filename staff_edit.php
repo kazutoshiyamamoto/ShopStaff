26ccee22980de46219c5df54c95903d3cc1fb6eb
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-29 00:01:54 +0900

    スタッフ修正画面の実装

--- staff_edit.php
+++ staff_edit.php
@@ -5,9 +5,7 @@
 <title>スタッフ修正</title>
 </head>
 <body>
-
 <?php
-
 try {
     $staff_code = $_POST['staffcode'];
 
@@ -30,10 +28,24 @@ try {
     print 'ただいま障害により大変ご迷惑をおかけしております。';
     exit();
 }
-
-
-
 ?>
-
+スタッフ修正<br />
+<br />
+スタッフコード<br />
+<?php print $staff_code; ?>
+<br />
+<br />
+<form method = "post" action = "staff_edit_check.php">
+<input type = "hidden" name = "code" value = "<?php print $staff_code; ?>">>
+スタッフ名<br />
+<input type = "text" name = "name" style = "width:200px" value = "<?php print $staff_name; ?>"><br />
+パスワードを入力してください。<br />
+<input type = "password" name = "pass" style = "width:100px"><br />
+パスワードをもう1度入力してください。<br />
+<input type = "password" name = "pass2" style = "width:100px"><br />
+<br />
+<input type = "button" onclick = "history.back()" value = "戻る">
+<input type = "submit" value = "OK">
+</form>
 </body>
 </html>
