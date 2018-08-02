639a32e08a48f70770f6dd016271850928e9a10f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-02 20:57:07 +0900

    処理の遷移先を変更

--- staff_delete.php
+++ staff_delete.php
@@ -39,15 +39,8 @@ try {
 <br />
 このスタッフを削除してよろしいでしょうか？<br />
 <br />
-<form method = "post" action = "staff_edit_check.php">
+<form method = "post" action = "staff_delete_done.php">
 <input type = "hidden" name = "code" value = "<?php print $staff_code; ?>">>
-スタッフ名<br />
-<input type = "text" name = "name" style = "width:200px" value = "<?php print $staff_name; ?>"><br />
-パスワードを入力してください。<br />
-<input type = "password" name = "pass" style = "width:100px"><br />
-パスワードをもう1度入力してください。<br />
-<input type = "password" name = "pass2" style = "width:100px"><br />
-<br />
 <input type = "button" onclick = "history.back()" value = "戻る">
 <input type = "submit" value = "OK">
 </form>
